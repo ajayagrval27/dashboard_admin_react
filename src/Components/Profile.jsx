04ef1bbcaba1c3fbd39467c5420684071b7b5042
ajayagrval27 <88595489+ajayagrval27@users.noreply.github.com>
2024-02-12 22:24:50 +0530

    serch_implitaton

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -155,6 +155,10 @@ const Profile = () => {
 		dispatch(getProfileData())
 	}, [dispatch])
 
+	useEffect(() => {
+		setFilteredData(profileData)
+	}, [profileData])
+
 	const handleFilter = (value) => {
 		console.log(value)
 		setSearchValue(value)
